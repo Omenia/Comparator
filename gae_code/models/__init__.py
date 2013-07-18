80853565e83e58cebfbb451e8b408dc943a4d32f
IsNoGood <ismo.aro@gmail.com>
2013-07-18 13:34:25 +0300

    moved databasmodels to the own directory.

---
+++ gae_code/models/__init__.py
@@ -0,0 +1,1 @@
+from databasemodels import *
---
+++ gae_code/models/databasemodels.py
@@ -0,0 +1,30 @@
+from google.appengine.ext import ndb
+
+class Grocery(ndb.Model):
+    name = ndb.StringProperty()
+    manufacturer = ndb.StringProperty()
+    price = ndb.FloatProperty()
+    quantity = ndb.StringProperty()
+    amount = ndb.FloatProperty()
+
+
+class Shop(ndb.Model):
+    """Models an individual shop"""
+    name = ndb.StringProperty()
+    city = ndb.StringProperty()
+    area = ndb.StringProperty()
+    postal_code = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now=True)
+    price = ndb.FloatProperty()
+    groceries = ndb.StructuredProperty(Grocery, repeated=True)
+
+    @classmethod
+    def query_book(cls, order = 'Halvin', qo = None):
+        if order == 'Halvin':
+            price_order = cls.price
+        else:
+            price_order = -cls.price
+        if qo:
+            return cls.query(qo).order(price_order)
+        else:
+            return cls.query().order(price_order)
