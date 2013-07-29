371c006c1c79ed1bd7f30c8bbd86d00585fe6ffc
IsNoGood <ismo.aro@gmail.com>
2013-07-29 19:17:34 +0300

    added forgotten file

---
+++ gae_code/models/staticdata.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+default_groceries = [
+					['Suomalainen tomaatti', '1 kg'], 
+					['Suomalainen rasvaton maito', '1 l'],
+					['Reissumies, Oululainen', '4 kpl'],
+					['Oltermanni, Valio', '1 kg'], 
+					['Suomalainen naudan jauheliha', '400 g'],
+					['Juhla Mokka kahvi, Paulig', '500 g'],
+					['Maustettu jogurtti, Arla Ingman', '1 l'],
+					['Tutti Frutti Mix Jumbo, Fazer', '400 g']
+					]
