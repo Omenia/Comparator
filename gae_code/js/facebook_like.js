3e7e456f6d1a10143e0d8973975d7030f866f933
Ismo Aro <ismo.aro@gmail.com>
2013-07-30 11:17:20 +0300

    moved facebook like to own .js file

---
+++ gae_code/js/facebook_like.js
@@ -0,0 +1,6 @@
+  (function() {
+    var e = document.createElement('script'); e.async = true;
+    e.src = document.location.protocol +
+      '//connect.facebook.net/fi_FI/all.js#xfbml=1';
+    document.getElementById('fb-root').appendChild(e);
+  }());
