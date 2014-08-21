f67a57e4949bacc969a0e452d3187e067f684c57
Juha Alhonen <juha.alhonen@omenia.fi>
2014-08-21 15:00:47 +0300

    app specifig code removed

--- gae_code/site/js/facebook_like.js
+++
@@ -1,6 +0,0 @@
-  (function() {
-    var e = document.createElement('script'); e.async = true;
-    e.src = document.location.protocol +
-      '//connect.facebook.net/fi_FI/all.js#xfbml=1';
-    document.getElementById('fb-root').appendChild(e);
-  }());
